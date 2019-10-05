11c6197c24f3acbd0f1290710a980d9fb97658c4
bendanPLUS <32793865+DuYuNeu@users.noreply.github.com>
2019-10-05 11:43:37 +0800

    剑指offer39题通过

--- src/test/java/com/bendanplus/algorithm/剑指offer/面试题39.java
+++ src/test/java/com/bendanplus/algorithm/剑指offer/面试题39.java
@@ -4,5 +4,32 @@ package com.bendanplus.algorithm.剑指offer;
  * 数组中出现次数超过一半的数字
  */
 public class 面试题39 {
+	public int MoreThanHalfNum_Solution(int[] array) {
+		if (array == null || array.length == 0) {
+			return 0;
+		}
+		int temp = array[0];
+		int time = 1;
+		int numbers = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (time == 0) {
+				temp = array[i];
+				time = 1;
+			} else if (temp == array[i]) {
+				time++;
 
+			} else {
+				time--;
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == temp) {
+				numbers++;
+			}
+		}
+		if (2 * numbers <= array.length) {
+			return 0;
+		}
+		return temp;
+	}
 }
