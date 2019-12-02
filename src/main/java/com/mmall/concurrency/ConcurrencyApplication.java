0c4efa8afc0307a13f912df9d0778c42e1630daa
duyu3 <duyuneu@163.com>
2019-12-02 16:14:50 +0800

    我能提交代码么？

--- src/main/java/com/mmall/concurrency/ConcurrencyApplication.java
+++ src/main/java/com/mmall/concurrency/ConcurrencyApplication.java
@@ -12,22 +12,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableCircuitBreaker
-public class ConcurrencyApplication extends WebMvcConfigurerAdapter{
+public class ConcurrencyApplication extends WebMvcConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConcurrencyApplication.class, args);
-	}
+    public static void main(String[] args) {
 
-	@Bean
-	public FilterRegistrationBean httpFilter() {
-		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-		registrationBean.setFilter(new HttpFilter());
-		registrationBean.addUrlPatterns("/threadLocal/*");
-		return registrationBean;
-	}
+        SpringApplication.run(ConcurrencyApplication.class, args);
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new HttpInterceptor()).addPathPatterns("/**");
-	}
+    }
+
+    @Bean
+    public FilterRegistrationBean httpFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new HttpFilter());
+        registrationBean.addUrlPatterns("/threadLocal/*");
+        return registrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HttpInterceptor()).addPathPatterns("/**");
+    }
 }
